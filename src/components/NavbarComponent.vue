6068b6f650eef54f69615155caa8bae81339a444
Chrystalio <chrystalio.k@outlook.com>
2024-02-13 21:27:17 +0700

    Add NavbarComponent

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="navbar py-3 navbar-expand-lg bg-body-tertiary shadow-sm">
+        <div class="container-fluid mx-lg-5">
+            <a class="navbar-brand fw-bold fs-5" href="#">PemiluPedia 📝</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <router-link to="/" class="text-decoration-none">
+                            <a class="nav-link active" aria-current="page">Home</a>
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
