131d70e2d566555b3b0a9ed3f8cbddfd02f937b4
Chrystalio <chrystalio.k@outlook.com>
2024-02-15 21:46:01 +0700

    Add Dapil Dropdown

---
+++ src/components/DapilDropdown.vue
@@ -0,0 +1,29 @@
+<template>
+    <select class="form-select" v-model="selectedDapil" @change="handleDistrictChange">
+        <option value="" class="text-center">Silahkan Memilih Dapil</option>
+        <option v-for="district in dapilList" :value="district.id" :key="district.id">{{ district.namaDapil }}
+        </option>
+    </select>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                selectedDapil: '',
+            };
+        },
+        props: {
+            dapilList: {
+                type: Array,
+                required: true,
+                validator: (value) => Array.isArray(value),
+            },
+        },
+        methods: {
+            handleDistrictChange() {
+                this.$emit('dapil-selected', this.selectedDapil);
+            },
+        },
+    };
+</script>
